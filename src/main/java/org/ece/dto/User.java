9f249bea5972536889f88966c487472b34b0ab3a
smatla <smatla@uwaterloo.ca>
2023-01-25 18:24:36 -0500

    TBDBANK-17- Integrated SingleStore online database and implemented basic crud operation

---
+++ src/main/java/org/ece/dto/User.java
@@ -0,0 +1,20 @@
+package org.ece.dto;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "User")
+public class User {
+
+    private String userId = UUID.randomUUID().toString();
+    @Id
+    private String userName;
+    @Enumerated(EnumType.STRING)
+    private AccessType accountType;
+    private String password;
+
+}
