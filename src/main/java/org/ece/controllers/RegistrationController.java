3e107c7e764322d85c582e27e740e9b9092f2b33
Jahnvi <jgdave@uwaterloo.ca>
2023-01-30 13:17:55 -0500

    Registration endpoint-changed the required method

--- src/main/java/org/ece/controllers/RegistrationController.java
+++ src/main/java/org/ece/controllers/RegistrationController.java
@@ -17,7 +17,7 @@ public class RegistrationController {
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest) {
         boolean result = registerService.validateRegisterRequest(registerRequest);
-        if (!result) {
+        if (result) {
             return new ResponseEntity<>("Thank you for Registering with TBD bank", HttpStatus.OK);
         } else {
             return new ResponseEntity<>("Username/SinNumber already exists, Please try again", HttpStatus.BAD_REQUEST);
--- src/main/java/org/ece/service/RegisterService.java
+++ src/main/java/org/ece/service/RegisterService.java
@@ -10,12 +10,8 @@ public class RegisterService {
     private static final String EXISTING_USER_NAME = "Test";
     private static final Long EXISTING_SIN_NUMBER = 123456789987L;
 
-    //validateRegisterRequest is checking for static values, it will check with DB values once integration is done
-    //It will return true if values are matching with EXISTING values
-    //In registrationController it is checking true or false
-    //If false then registration will be successful
     public boolean validateRegisterRequest(RegisterRequest registerRequest) {
-        return EXISTING_USER_NAME.equals(registerRequest.getUserName())
-                && EXISTING_SIN_NUMBER.equals(registerRequest.getSinNumber());
+        return !(EXISTING_USER_NAME.equals(registerRequest.getUserName())
+                && EXISTING_SIN_NUMBER.equals(registerRequest.getSinNumber()));
     }
 }
