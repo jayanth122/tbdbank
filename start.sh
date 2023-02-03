c51ad30cf8626771de01d5f9e3e92760b997481c
= <=>
2023-02-03 04:42:52 -0500

    TBDBANK-25-docker infra adding docker changes for Ui and nginx

--- start.sh
+++ start.sh
@@ -1,3 +1,27 @@
 #! /bin/bash
-./gradlew -stop ; ./gradlew bootrun &
-cd tbdfrontend || exit ; ng serve --port 8080
+#./gradlew -stop ; ./gradlew bootrun &
+#cd tbdfrontend || exit ; ng serve --port 8080
+
+
+
+#Build frontend
+AngularBuild(){
+    cd tbdfrontend
+    echo "building angular"
+    npm cache clean --force
+    npm install
+    npm run build   --dev
+    cd ..
+
+    
+}
+
+BuildDocker(){
+    cd docker/dockercompose
+    docker-compose build --no-cache --progress=plain
+    docker compose up -d
+
+}
+
+AngularBuild
+BuildDocker
