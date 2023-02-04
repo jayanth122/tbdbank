725a0578631b97ac8b552284da0348ad8413a4e4
njchennu <njchennu@uwaterloo.ca>
2023-02-03 22:25:05 -0500

    TBDBANK-11 Refactored Transaction Services

---
+++ src/test/java/org/ece/service/TransactionServiceTest.java
@@ -0,0 +1,6 @@
+package org.ece.service;
+
+public class TransactionServiceTest {
+    private static final String SAMPLE_AMOUNT = "100";
+    private static final String SAMPLE_CUSTOMER_ID = "SAMPLE_CUSTOMER_ID";
+}
