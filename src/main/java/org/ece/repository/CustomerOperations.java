bc607ce4c43628d95ac316fe5f6d276933f4426c
Jahnvi <jgdave@uwaterloo.ca>
2023-02-07 23:45:44 -0500

    CustomerDatabase-Integration with UserName and SinNumber Validation

---
+++ src/main/java/org/ece/repository/CustomerOperations.java
@@ -0,0 +1,13 @@
+package org.ece.repository;
+
+import org.ece.dto.Customer;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface CustomerOperations extends CrudRepository<Customer, String> {
+    Optional<Customer> findByUserName(String username);
+    Optional<Customer> findBySinNumber(Long sinNumber);
+}
