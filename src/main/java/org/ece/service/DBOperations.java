3b1ba46d60dd5b4cc601b2c2991bd39e4338b7a3
smatla <smatla@uwaterloo.ca>
2023-02-09 18:55:05 -0500

    TBDBANK-36 Fetching customer data for login session

--- src/main/java/org/ece/service/DBOperations.java
+++ src/main/java/org/ece/service/DBOperations.java
@@ -1,7 +1,9 @@
 package org.ece.service;
 
+import org.ece.dto.Customer;
 import org.ece.dto.Manager;
 import org.ece.dto.User;
+import org.ece.repository.CustomerOperations;
 import org.ece.repository.ManagerOperations;
 import org.ece.repository.UserOperations;
 import org.springframework.stereotype.Service;
@@ -11,11 +13,14 @@ public class DBOperations {
 
     private UserOperations userOperations;
     private ManagerOperations managerOperations;
+    private final CustomerOperations customerOperations;
 
 
-    public DBOperations(final ManagerOperations managerOperations, final UserOperations userOperations) {
+    public DBOperations(final ManagerOperations managerOperations, final UserOperations userOperations,
+                        final CustomerOperations customerOperations) {
         this.managerOperations = managerOperations;
         this.userOperations = userOperations;
+        this.customerOperations = customerOperations;
     }
 
     private User setManagerDetailsInUser(final User user) {
@@ -26,10 +31,20 @@ public class DBOperations {
         return user;
     }
 
+    private User setCustomerDetailsInUser(final User user) {
+        Customer customer = customerOperations.findByUserName(user.getUserName()).get();
+        user.setFirstName(customer.getFirstName());
+        user.setLastName(customer.getLastName());
+        user.setUserId(customer.getCustomerId());
+        return user;
+    }
+
     public User getUserDetails(final User user) {
         switch (user.getAccountType()) {
             case MANAGER:
                 return setManagerDetailsInUser(user);
+            case CUSTOMER:
+                return setCustomerDetailsInUser(user);
             default:
                 return null;
         }
