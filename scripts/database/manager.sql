cb524cb18eaac43bde73fa1662cad9b01e69cfec
smatla <smatla@uwaterloo.ca>
2023-02-09 19:17:17 -0500

    TBDBANK-35 created sql script for manager table and added test cases for operations

---
+++ scripts/database/manager.sql
@@ -0,0 +1,7 @@
+CREATE TABLE manager
+(
+    manager_id varchar(50) PRIMARY KEY,
+    user_name  varchar(25) NOT NULL,
+    first_name varchar(20) NOT NULL,
+    last_name  varchar(20) NOT NULL
+);
--- src/test/java/org/ece/repository/ManagerOperationsTest.java
+++ src/test/java/org/ece/repository/ManagerOperationsTest.java
@@ -1,6 +1,8 @@
 package org.ece.repository;
 
+import org.ece.dto.AccessType;
 import org.ece.dto.Manager;
+import org.ece.dto.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class ManagerOperationsTest {
 
     private static final String EXISTING_USER_NAME = "manager_san";
+    private static final String SAMPLE_PASSWORD = "encrypted_password";
 
     @Autowired
     UserOperations userOperations;
@@ -23,10 +26,24 @@ public class ManagerOperationsTest {
 
     @Test
     public void testFindManagerByUserName() {
+        Assertions.assertTrue(managerOperations.findManagerByUserName(EXISTING_USER_NAME).isPresent());
+    }
+
+    @Test
+    public void testSaveManagerRecordToDB() {
         Manager manager = new Manager();
-        manager.setUserName(EXISTING_USER_NAME);
+        manager.setUserName(EXISTING_USER_NAME); // Change tp required User_Name
         manager.setFirstName("san");
         manager.setLastName("test");
-        Assertions.assertTrue(managerOperations.findManagerByUserName(EXISTING_USER_NAME).isPresent());
+        if (!userOperations.findById(manager.getUserName()).isPresent()) {
+           User user = new User();
+           user.setUserName(manager.getUserName());
+           user.setAccountType(AccessType.MANAGER);
+           user.setPassword(SAMPLE_PASSWORD);
+           userOperations.save(user);
+           managerOperations.save(manager);
+           Assertions.assertTrue(managerOperations.findManagerByUserName(EXISTING_USER_NAME).isPresent());
+        }
+        System.out.println("User Name Already Exists");
     }
 }
