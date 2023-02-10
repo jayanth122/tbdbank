1402164db1870d4d32d55c09fe5dccfe14b815f7
njchennu <njchennu@uwaterloo.ca>
2023-02-10 15:45:34 -0500

    TBDBANK-29 code Refactor

--- src/test/java/org/ece/repository/TransactionOperationsTest.java
+++ src/test/java/org/ece/repository/TransactionOperationsTest.java
@@ -11,13 +11,12 @@ import org.ece.dto.TransactionType;
 import org.ece.dto.Transaction;
 
 /**
- * Tests for user CRUD operations.
+ * Tests for transaction CRUD operations.
  * Tests are excluded in gradle file, uncomment the exlcude line to run test on local
  */
 @SpringBootTest
 public class TransactionOperationsTest {
     private static final String EXISTING_TRANSACTION_ID = "76dcc251-fafb-4080-89a0-2c151f777328";
-    //private static final String TEST_SAVE_TRANSACTION_ID = "test_transaction_id";
     private static final String TEST_SAVE_CUSTOMER_ID = "test_customer_id";
     private static final String TEST_SAVE_BALANCE = "10001";
     private static final String TEST_SAVE_AMOUNT = "1";
@@ -31,19 +30,12 @@ public class TransactionOperationsTest {
         System.out.println("Executing ignored test");
         Transaction transaction = new Transaction();
         transaction.setTransactionId(EXISTING_TRANSACTION_ID);
-        //transaction.setTransactionType(TransactionType.DEBIT);
         transaction.setCustomerId("test_customer_id");
         Assertions.assertTrue(transactionOperations.findById(transaction.getTransactionId()).isPresent());
     }
 
     @Test
     public void testSaveTransaction() {
-        //DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu/MM/dd");
-        //LocalDateTime localDateTime = LocalDateTime.now();
-        //LocalTime localTime = LocalTime.now();
-        //DateTime dateTime = java.sql.Date.valueOf(localDateTime);
-        //Time time = java.sql.Time.valueOf(localTime);
-        //System.out.println(dtf.format(localDate));
         System.out.println("Executing ignored test");
         Transaction transaction = new Transaction();
         transaction.setTransactionId(transaction.getTransactionId());
