298a4ae01d374a189ac941688d73b8810b52bac1
smatla <smatla@uwaterloo.ca>
2023-02-16 22:24:22 -0500

    TBDBANK-41 Rejecting multiple duplicate login requests

--- src/main/java/org/ece/service/CacheService.java
+++ src/main/java/org/ece/service/CacheService.java
@@ -1,9 +1,12 @@
 package org.ece.service;
 
+import org.apache.commons.lang3.StringUtils;
+import org.ece.dto.AccessType;
 import org.ece.dto.SessionData;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -11,21 +14,33 @@ public class CacheService {
     private static final int TIME_TO_LIVE_FOR_SESSION_DATA_IN_MINUTES = 5;
 
     private final RedisTemplate<String, SessionData> redisTemplate;
+    private static final String SESSION_DATA_PREFIX = "TBD651:";
 
     public CacheService(final RedisTemplate<String, SessionData> redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
     public void createSession(final String uniqueSessionId, final SessionData sessionData) {
-        redisTemplate.opsForValue().set(uniqueSessionId, sessionData, TIME_TO_LIVE_FOR_SESSION_DATA_IN_MINUTES,
+        redisTemplate.opsForValue().set(SESSION_DATA_PREFIX + uniqueSessionId, sessionData,
+                TIME_TO_LIVE_FOR_SESSION_DATA_IN_MINUTES,
                 TimeUnit.MINUTES);
     }
 
     public SessionData validateSession(final String uniqueSessionId) {
-       return redisTemplate.hasKey(uniqueSessionId) ? redisTemplate.opsForValue().get(uniqueSessionId) : null;
+        return redisTemplate.hasKey(SESSION_DATA_PREFIX + uniqueSessionId)
+                ? redisTemplate.opsForValue().get(uniqueSessionId) : null;
     }
 
     public void killSession(final String uniqueSessionId) {
         redisTemplate.delete(uniqueSessionId);
+        redisTemplate.opsForValue();
+    }
+
+    public boolean doesUserSessionExist(final AccessType accessType, final String userId) {
+        Set<String> keys = redisTemplate.keys(SESSION_DATA_PREFIX + "*");
+        return keys.stream().map(key -> redisTemplate.opsForValue().get(key))
+                .filter(sessionData -> sessionData.getAccessType().equals(accessType))
+                .map(sessionData -> StringUtils.equals(sessionData.getUserId(), userId))
+                .findFirst().isPresent();
     }
 }
