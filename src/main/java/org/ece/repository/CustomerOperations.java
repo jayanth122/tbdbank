97d006f24d53b3893ca664df706348d7afb6b390
njchennu <njchennu@uwaterloo.ca>
2023-02-23 21:19:08 -0500

    TBDBANK42 Debveloped generateStatement

--- src/main/java/org/ece/repository/CustomerOperations.java
+++ src/main/java/org/ece/repository/CustomerOperations.java
@@ -1,13 +1,20 @@
 package org.ece.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.ece.dto.Customer;
-
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 @Repository
 public interface CustomerOperations extends CrudRepository<Customer, String> {
     Optional<Customer> findByUserName(String username);
     Optional<Customer> findBySinNumber(Long sinNumber);
+
+    @Query("SELECT d FROM Customer d WHERE d.customerId like :customerId")
+    List<Customer> findCustomerDetailsByCustomerId(@Param("customerId") String customerId);
+
+
 }
