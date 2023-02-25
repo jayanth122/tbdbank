ef4dbd2e80c3f741cc3b779bb0f515a3d3a3782b
njchennu <njchennu@uwaterloo.ca>
2023-02-25 12:29:46 -0500

    TBDBANK-29 Refactored ConversionUtils -- Using BigDecimal

--- src/main/java/org/ece/util/ConversionUtils.java
+++ src/main/java/org/ece/util/ConversionUtils.java
@@ -1,18 +1,17 @@
 package org.ece.util;
 
 
+import java.math.BigDecimal;
+
 public final class ConversionUtils {
-    public static Long convertToLong(final String amount) {
-        Long amountL = Long.valueOf(amount);
-        return amountL;
+    public static Long upScale(final BigDecimal amount) {
+        amount.multiply(BigDecimal.valueOf(100,2));
+        return amount.longValue();
     }
 
-    private ConversionUtils() {
-        throw new IllegalStateException("Utility class");
+    public static BigDecimal downScale(final Long amount){
+        BigDecimal d = BigDecimal.valueOf(amount,2);
+        d.divide(BigDecimal.valueOf(100));
+        return d;
     }
-    public static String convertToString(final Long amount) {
-        String amountS = String.valueOf(amount);
-        return amountS;
-    }
-
 }
