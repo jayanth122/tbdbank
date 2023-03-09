fe0dd9c8b27fe923e19ad34214b7b1fcff48579c
njchennu <njchennu@uwaterloo.ca>
2023-03-09 16:32:55 -0500

    TBDBANK-59 Refactored CustomerPayee

--- src/main/java/org/ece/dto/CustomerPayee.java
+++ src/main/java/org/ece/dto/CustomerPayee.java
@@ -5,12 +5,13 @@ import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.UUID;
 
 @Data
 @Entity
 @Table(name = "customerpayee")
 public class CustomerPayee {
     @Id
-    private String payeeName;
+    private String payeeName = UUID.randomUUID().toString();
     private String accountNumber;
 }
