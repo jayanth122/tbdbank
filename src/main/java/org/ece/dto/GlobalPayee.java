94c2cca671f0ea1c626e4f7c74e891217ec52158
njchennu <njchennu@uwaterloo.ca>
2023-03-22 13:12:11 -0400

    TBDBANK-59 Updated globalpayee code

--- src/main/java/org/ece/dto/GlobalPayee.java
+++ src/main/java/org/ece/dto/GlobalPayee.java
@@ -5,13 +5,14 @@ import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.UUID;
 
 @Data
 @Entity
 @Table(name = "globalpayee")
 public class GlobalPayee {
     @Id
-    private String payeeId;
+    private String payeeId = UUID.randomUUID().toString();;
     private String payeeName;
     private String email;
     private String bankName;
