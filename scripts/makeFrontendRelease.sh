94c535040dbe7dfeabb822fb1d288fcd9d8467f1
smatla <smatla@uwaterloo.ca>
2023-03-25 19:29:52 -0400

    Frontend release script

--- scripts/makeFrontendRelease.sh
+++ scripts/makeFrontendRelease.sh
@@ -1,7 +1,8 @@
 ssh -i santhoshpvtkey.txt ubuntu@132.145.103.186 'cd /home/ubuntu/tbdbank/tbdbank &&
  git checkout main &&
   git fetch && git pull &&
-   cd tbdfrontend
+   cd tbdfrontend &&
+
    container_id=$(docker ps -q --filter "publish=8080")
 
   # Stop the container
@@ -9,5 +10,5 @@ ssh -i santhoshpvtkey.txt ubuntu@132.145.103.186 'cd /home/ubuntu/tbdbank/tbdban
     docker stop "$container_id"
     docker rm "$container_id"
   fi &&
-   docker build -t tbdbankFrontend-image . &&
-    docker run -d --net san_network --name tbdbankFrontend-container -p 8080:8080 tbdbankFrontend-image'
+   docker build -t tbdbankfrontend-image . &&
+    docker run --net san_network --name tbdbankfrontend-container -p 8080:8080 tbdbankfrontend-image'
