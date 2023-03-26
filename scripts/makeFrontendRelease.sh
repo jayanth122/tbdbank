acc2f5a5eb3789f75d6d8af29a752c7e20fa9350
smatla <smatla@uwaterloo.ca>
2023-03-25 20:42:33 -0400

    Fixed frontend release script

--- scripts/makeFrontendRelease.sh
+++ scripts/makeFrontendRelease.sh
@@ -1,9 +1,14 @@
 ssh -i santhoshpvtkey.txt ubuntu@132.145.103.186 'cd /home/ubuntu/tbdbank/tbdbank &&
  git checkout main &&
   git fetch && git pull &&
-   cd tbdfrontend &&
+  cd /usr/share/nginx/html
+       if [ -d "tbdfrontend" ]; then
+         echo "Directory exists, deleting..."
+         sudo rm -r tbdfrontend
+       fi
+   cd /home/ubuntu/tbdbank/tbdbank/tbdfrontend &&
 
-   container_id=$(docker ps -q --filter "publish=8080")
+   container_id=$(docker ps -aq -f name=tbdbankfrontend-container)
 
   # Stop the container
   if [ ! -z "$container_id" ]; then
@@ -11,4 +16,5 @@ ssh -i santhoshpvtkey.txt ubuntu@132.145.103.186 'cd /home/ubuntu/tbdbank/tbdban
     docker rm "$container_id"
   fi &&
    docker build -t tbdbankfrontend-image . &&
-    docker run --net san_network --name tbdbankfrontend-container -p 8080:8080 tbdbankfrontend-image'
+   docker run -d -p 8080:80 --name tbdbankfrontend-container tbdbankfrontend-image
+   sudo docker cp tbdbankfrontend-container:/frontendApp/dist/tbdfrontend /usr/share/nginx/html'
