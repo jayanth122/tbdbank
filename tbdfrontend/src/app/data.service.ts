be8a79717482fe3c724bbde4e9545bafa2b120d6
Jahnvi <jgdave@uwaterloo.ca>
2023-03-28 14:03:24 -0400

    TBDBANK-45-updated dataservice file

--- tbdfrontend/src/app/data.service.ts
+++ tbdfrontend/src/app/data.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs'
+import {QrRequest} from "./dto/QrRequest";
+import {StatementRequest} from "./dto/StatementRequest";
 
 
 @Injectable({
