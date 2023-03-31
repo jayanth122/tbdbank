8f2c0469f3ff670e5f5c043358b26f36b59eaa78
smatla <smatla@uwaterloo.ca>
2023-03-31 18:03:36 -0400

    Added Web Security

--- src/main/java/org/ece/configuration/WebConfig.java
+++ src/main/java/org/ece/configuration/WebConfig.java
@@ -6,8 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig {
+    private static final String TBDBANK_DOMAIN = "https://www.tbdbank.me";
+    private static final String TBDBANK_DOMAIN_2 = "tbdbank.me";
+    private static final String SANTHOSH_DOMAIN = "https://www.santhoshprojects.me";
+    private static final String SANTHOSH_DOMAIN_2 = "santhoshprojects.me";
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -16,7 +22,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowCredentials(true)
-                        .allowedOriginPatterns("*");
+                        .allowedOrigins(TBDBANK_DOMAIN, TBDBANK_DOMAIN_2, SANTHOSH_DOMAIN, SANTHOSH_DOMAIN_2);
             }
         };
     }
