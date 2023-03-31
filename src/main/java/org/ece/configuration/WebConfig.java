b6e569e88f38dad19835dbbc41b6b4d356ab5184
smatla <smatla@uwaterloo.ca>
2023-03-31 18:20:39 -0400

    Anirudh IP Fix

--- src/main/java/org/ece/configuration/WebConfig.java
+++ src/main/java/org/ece/configuration/WebConfig.java
@@ -13,6 +13,8 @@ public class WebConfig {
     private static final String SANTHOSH_DOMAIN = "https://www.santhoshprojects.me";
     private static final String SANTHOSH_DOMAIN_2 = "santhoshprojects.me";
     private static final String ANIRUDH_IP = "192.168.0.28:4200";
+    private static final String ANIRUDH_IP_2 = "192.168.0.28";
+    private static final String ANIRUDH_IP_3 = "localhost:4200";
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -22,7 +24,7 @@ public class WebConfig {
                 registry.addMapping("/**")
                         .allowCredentials(true)
                         .allowedOrigins(TBDBANK_DOMAIN, TBDBANK_DOMAIN_2, SANTHOSH_DOMAIN, SANTHOSH_DOMAIN_2,
-                                ANIRUDH_IP);
+                                ANIRUDH_IP, ANIRUDH_IP_2, ANIRUDH_IP_3);
             }
         };
     }
