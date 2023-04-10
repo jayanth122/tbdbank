d969fc8ec3db7cc11dd016985e661d69ffe809e2
Jahnvi <jgdave@uwaterloo.ca>
2023-04-10 02:19:16 -0400

    TBDBANK113-few test cases

--- tbdfrontend/src/app/data.service.spec.ts
+++ tbdfrontend/src/app/data.service.spec.ts
@@ -1,7 +1,7 @@
 import {inject, TestBed} from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-
+import {Router, RouterModule} from '@angular/router';
 import { DataService } from './data.service';
 import { QrRequest } from './dto/QrRequest';
 import { StatementRequest } from './dto/StatementRequest';
@@ -10,13 +10,15 @@ import { UserDetailsRequest } from './dto/UserDetailsRequest';
 import { LogOutRequest } from './dto/LogOutRequest';
 import { RefreshRequest } from './dto/RefreshRequest';
 import { UpiPaymentRequest } from './dto/UpiPaymentRequest';
-import { of } from 'rxjs';
+import {of, throwError} from 'rxjs';
 import { DatePipe } from '@angular/common';
 
 describe('DataService', () => {
   let service: DataService;
   let httpMock: HttpTestingController;
   let httpClientSpy: { post: jasmine.Spy };
+  let router: Router;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -26,6 +28,8 @@ describe('DataService', () => {
     service = TestBed.inject(DataService);
     httpMock = TestBed.inject(HttpTestingController);
     httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);
+
+
   });
 
   it('should be created', () => {
@@ -136,7 +140,165 @@ describe('DataService', () => {
     const retrievedStatementPdfData = service.getStatementPdf();
     expect(retrievedStatementPdfData).toEqual(statementPdfData);
   });
-  it('should generate QR code', () => {
+  it('should call generateQr with the correct request', () => {
+    const qrRequest: QrRequest = { sessionId: 'some data' };
+    httpClientSpy.post.and.returnValue(of({ success: true }));
+
+    service.generateQr(qrRequest).subscribe(() => {
+      expect(httpClientSpy.post).toHaveBeenCalledWith(`${service['url']}/qr/generateQR`, qrRequest);
+    });
+  });
+
+  it('should call fetchUserDetails with the correct request', () => {
+    const userDetailsRequest: UserDetailsRequest = { sessionId: 'qert' };
+    httpClientSpy.post.and.returnValue(of({ success: true }));
+
+    service.fetchUserDetails(userDetailsRequest).subscribe(() => {
+      expect(httpClientSpy.post).toHaveBeenCalledWith(`${service['url']}/customer`, userDetailsRequest);
+    });
+  });
+
+  it('should send interac validate request to the correct endpoint with correct data', () => {
+    const interacValidateRequest: InteracValidateRequest = {
+      sessionId: 'some data',
+      email:'test@test.com'
+    };
+    httpClientSpy.post.and.returnValue(of({ success: true }));
+
+    service.validateInterac(interacValidateRequest).subscribe(() => {
+      expect(httpClientSpy.post).toHaveBeenCalledWith(
+        `${service['url']}/transaction/interac/validate`,
+        interacValidateRequest
+      );
+    });
+  });
+
+  it('should return an observable of success response after sending interac validate request', () => {
+    const interacValidateRequest: InteracValidateRequest = {
+      sessionId: 'some data',
+      email:'test@test.com'
+    };
+    const expectedResponse = { success: true };
+    httpClientSpy.post.and.returnValue(of(expectedResponse));
+
+    service.validateInterac(interacValidateRequest).subscribe((response) => {
+      expect(response).toEqual(expectedResponse);
+    });
+  });
+
+  it('should return user details on successful request', () => {
+    const userDetailsRequest: UserDetailsRequest = { sessionId: 'some data' };
+    const userDetailsResponse = { name: 'John Doe', email: 'johndoe@example.com' };
+    httpClientSpy.post.and.returnValue(of(userDetailsResponse));
+    service.fetchUserDetails(userDetailsRequest).subscribe((response) => {
+      expect(response).toEqual(userDetailsResponse);
+    });
+  });
+
+  it('should handle errors', () => {
+    const userDetailsRequest: UserDetailsRequest = { sessionId: 'some data' };
+    const errorResponse = { status: 500, message: 'Internal Server Error' };
+    httpClientSpy.post.and.returnValue(throwError(errorResponse));
+    service.fetchUserDetails(userDetailsRequest).subscribe(
+      (response) => {},
+      (error) => {
+        expect(error).toEqual(errorResponse);
+      }
+    );
+  });
+  it('should update session and store account balance and email when user details are fetched successfully', () => {
+    // Arrange
+    const sessionId = 'session123';
+    const customer = {
+      accountBalance: 1000,
+      email: 'test@example.com'
+    };
+    const response = {
+      success: true,
+      sessionId: 'newSessionId',
+      customer: customer
+    };
+    spyOn(localStorage, 'getItem').and.returnValue(sessionId);
+    spyOn(service, 'fetchUserDetails').and.returnValue(of(response));
+    spyOn(localStorage, 'setItem');
+    spyOn(service, 'updateSession');
+
+    // Act
+    service.fetchCustomerDetails();
+
+    // Assert
+    expect(service.fetchUserDetails).toHaveBeenCalledWith({ sessionId: sessionId });
+    expect(service.updateSession).toHaveBeenCalledWith(true, 'newSessionId');
+    expect(localStorage.setItem).toHaveBeenCalledWith('accountBalance', JSON.stringify(customer.accountBalance));
+    expect(localStorage.setItem).toHaveBeenCalledWith('email', JSON.stringify(customer.email));
+  });
+
+  it('should call logOut with the correct request', () => {
+    // Arrange
+    const logOutRequest: LogOutRequest = { sessionId: 'some session id' };
+    httpClientSpy.post.and.returnValue(of({ success: true }));
+
+    // Act
+    service.logOut(logOutRequest).subscribe(() => {
+
+      // Assert
+      expect(httpClientSpy.post).toHaveBeenCalledWith(`${service['url']}/logout`, logOutRequest, {responseType: "text"});
+    });
+  });
+
+  it('should make a UPI payment request', () => {
+    // Arrange
+    const textEncoder = new TextEncoder();
+    const qrImage = "Hello, world!";
+    const myUint8Array = textEncoder.encode(qrImage);
+
+    const upiPaymentRequest: UpiPaymentRequest = {
+      sessionId: 'some session id',
+      qrImage: myUint8Array
+    };
+    httpClientSpy.post.and.returnValue(of({success: true}));
+
+    // Act
+
+    service.upiPayment(upiPaymentRequest).subscribe(() => {
+      // Assert
+      expect(httpClientSpy.post).toHaveBeenCalledWith(`${service['url']}/qr/QRPayment`, upiPaymentRequest);
+    });
+  });
+
+  it('should set isLoginValid and sessionId in localStorage and start timeout', () => {
+    // Arrange
+    spyOn(localStorage, 'setItem');
+    spyOn(window, 'setTimeout');
+    const isValid = true;
+    const newSessionId = '12345';
+
+    // Act
+    service.setIsLoginValid(isValid, newSessionId);
+
+    // Assert
+    expect(service.isLoginValid).toBe(isValid);
+    expect(localStorage.setItem).toHaveBeenCalledWith('sessionId', newSessionId);
+    expect(window.setTimeout).toHaveBeenCalled();
+  });
+  it('should return the verification image', () => {
+    // Arrange
+    const verificationImage = 'data:image/png;base64,iVBORw0KGg...'; // base64-encoded image data
+    service.verificationImage = verificationImage;
+
+    // Act
+    const result = service.getVerificationImage();
+
+    // Assert
+    expect(result).toEqual(verificationImage);
+  });
+
+});
+
+
+
+
+  /*it('should generate QR code', () => {
     const qrRequest: QrRequest = {
       amount: 100,
       currency: 'USD',
@@ -153,7 +315,7 @@ describe('DataService', () => {
     expect(httpClientSpy.post.calls.count()).toBe(1, 'one call');
   });
 
+*/
 
 
 
-});
