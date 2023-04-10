2bc7665ac8dc22e04d23ddc47328664d7092f287
Jahnvi <jgdave@uwaterloo.ca>
2023-04-10 02:39:41 -0400

    Fixed error

--- tbdfrontend/src/app/components/upi-payments/upi-payments.component.spec.ts
+++ tbdfrontend/src/app/components/upi-payments/upi-payments.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { UpiPaymentsComponent } from './upi-payments.component';
-
+import {HeaderComponent} from "../header/header.component";
+import {FooterComponent} from "../footer/footer.component";
 describe('UpiPaymentsComponent', () => {
   let component: UpiPaymentsComponent;
   let fixture: ComponentFixture<UpiPaymentsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UpiPaymentsComponent],
+      declarations: [
+        UpiPaymentsComponent,
+        HeaderComponent,
+        FooterComponent
+      ],
       imports: [HttpClientTestingModule]
     })
     .compileComponents();
