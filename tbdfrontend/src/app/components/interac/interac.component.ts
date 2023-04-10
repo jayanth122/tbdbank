8a6ec69c5812462e854c389bbcfd4b90a46ab123
smatla <smatla@uwaterloo.ca>
2023-04-10 02:44:58 -0400

    UPI payment fix

--- tbdfrontend/src/app/components/interac/interac.component.ts
+++ tbdfrontend/src/app/components/interac/interac.component.ts
@@ -25,6 +25,7 @@ export class InteracComponent implements OnInit, OnDestroy {
   submitted = false;
   isBtnDisabled=true;
   isEmailValid=false;
+  isUpiPayment=false;
   private readonly destroy$: Subject<void> = new Subject();
 
   constructor(
@@ -52,9 +53,11 @@ export class InteracComponent implements OnInit, OnDestroy {
     this.enableSubmit();
     console.log("................................",this.dataService.getTPartyIntracEmail)
     if (this.dataService.getTPartyIntracEmail().length>0){
+      this.isUpiPayment = true;
     this.interacForm.patchValue({
   receiverEmail: this.dataService.getTPartyIntracEmail()
 });
+      this.interacForm.get('receiverEmail')?.disable();
     }
   this.onchangeUsername()
 
@@ -120,6 +123,9 @@ export class InteracComponent implements OnInit, OnDestroy {
         } else {
           this.notLinked = true;
           this.interacEmailError = 'Email not linked to any bank account';
+          if (data.message === 'Cannot send to Self Account') {
+            this.interacEmailError = 'Cannot send to Self Account';
+          }
           this.amountEnabled = false;
           this.dataService.updateSession(true, data.sessionId);
           this.interacForm.get('amount')?.disable();
@@ -127,7 +133,7 @@ export class InteracComponent implements OnInit, OnDestroy {
       });
   }
 
-  validateEmail(email: string): boolean { 
+  validateEmail(email: string): boolean {
     const emailRegex = /^([a-zA-Z0-9._%+-]+)@([a-zA-Z]{5,10})\.([a-zA-Z]{3})$/;
     return emailRegex.test(email);
   }
